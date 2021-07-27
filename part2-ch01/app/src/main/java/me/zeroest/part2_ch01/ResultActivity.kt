062917a553820df8f91153b944777c1b84aa8fd2
kyd <husheart@naver.com>
2021-07-28 01:15:45 +0900

    bmi refactoring

--- part2-ch01/app/src/main/java/me/zeroest/part2_ch01/ResultActivity.kt
+++ part2-ch01/app/src/main/java/me/zeroest/part2_ch01/ResultActivity.kt
@@ -6,7 +6,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.pow
 
-class ResultActivity: AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +17,22 @@ class ResultActivity: AppCompatActivity() {
 
         Log.d("ResultActivity", "height: $height, weight: $weight")
 
-        val bmi = weight / (height / 100.0).pow(2.0)
-        val resultText = when {
+        val bmi = getBmi(height, weight)
+
+        val resultValueTextView = findViewById<TextView>(R.id.bmiTextView)
+        val resultStringTextView = findViewById<TextView>(R.id.resultTextView)
+
+        resultValueTextView.text = bmi.toString()
+        resultStringTextView.text = getResultText(bmi)
+
+    }
+
+    private fun getBmi(height: Int, weight: Int): Double {
+        return weight / (height / 100.0).pow(2.0)
+    }
+
+    private fun getResultText (bmi: Double): String {
+        return when {
             bmi >= 35.0 -> "고도 비만"
             bmi >= 30.0 -> "중정도 비만"
             bmi >= 25.0 -> "경도 비만"
@@ -26,13 +40,6 @@ class ResultActivity: AppCompatActivity() {
             bmi >= 18.5 -> "정상체중"
             else -> "저체중"
         }
-
-        val resultValueTextView = findViewById<TextView>(R.id.bmiTextView)
-        val resultStringTextView = findViewById<TextView>(R.id.resultTextView)
-
-        resultValueTextView.text = bmi.toString()
-        resultStringTextView.text = resultText
-
     }
 
 }
