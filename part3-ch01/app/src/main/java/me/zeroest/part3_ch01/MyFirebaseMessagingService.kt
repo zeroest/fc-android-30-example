0747c59ef41259b86f947fe9df0e5cc382793a94
kyd <husheart@naver.com>
2021-07-30 03:27:18 +0900

    Firebase messaging - create noti

--- part3-ch01/app/src/main/java/me/zeroest/part3_ch01/MyFirebaseMessagingService.kt
+++ part3-ch01/app/src/main/java/me/zeroest/part3_ch01/MyFirebaseMessagingService.kt
@@ -1,7 +1,14 @@
 package me.zeroest.part3_ch01
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -17,10 +24,55 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
     }
 
     // 실제 푸시가 왔을때 호출되는 메서드
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
 
-        Log.e("FirebaseMessageingService", "message : ${message.data.toString()}")
+        Log.e("FirebaseMessageingService", "message : ${remoteMessage.data.toString()}")
+
+        val type = remoteMessage.data["type"]
+            ?.let { NotificationType.valueOf(it) }
+        val title = remoteMessage.data["title"]
+        val message = remoteMessage.data["message"]
+
+        type ?: return
+
+        createNotificationChannel()
+
+        // 노티를 직접 띄운다.
+        NotificationManagerCompat.from(this)
+            .notify(1, createNotification(title, message))
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = CHANNEL_DESCRIPTION
+
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+        }
+    }
+
+    private fun createNotification(
+        title: String?,
+        message: String?
+    ): Notification {
+        return NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_waves_24)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+    }
+
+    companion object {
+        private const val CHANNEL_NAME = "Emoji Party"
+        private const val CHANNEL_DESCRIPTION = "Emoji Party를 위한 채널"
+        private const val CHANNEL_ID = "Channel Id"
     }
 
 }
---
+++ part3-ch01/app/src/main/java/me/zeroest/part3_ch01/NotificationType.kt
@@ -0,0 +1,8 @@
+package me.zeroest.part3_ch01
+
+enum class NotificationType(val title: String, val id: Int) {
+    NORMAL("일반 알림", 0),
+    EXPANDABLE("확장형 알림", 1),
+    CUSTOM("커스텀 알림", 2)
+
+}
